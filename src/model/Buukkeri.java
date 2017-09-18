b1151ab400b48e5f46b5c7e9dcf523fbd0fe679d
romppane <ronim@metropolia.fi>
2017-09-18 11:56:42 +0300

    merge devistä

--- src/model/Buukkeri.java
+++ src/model/Buukkeri.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Buukkeri {
-
+//ASD
 	public static void main(String[] args) {
 		DAO_IF test = new DAO();
 		SP pena = new SP("vuorot", "pena", "ronim@metropolia.fi", "0405353169");
