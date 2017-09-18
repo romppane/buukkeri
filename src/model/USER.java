9bfbe7c682336a50a4b406eb49298674beb69512
Marika <nokelainen.marika@gmail.com>
2017-09-18 12:12:39 +0300

    user class

---
+++ src/model/USER.java
@@ -0,0 +1,12 @@
+package model;
+
+public class USER {
+	private int id;
+	private String fname;
+	private String lname;
+	private String password;
+	private String phone;
+	private String email;
+	
+	
+}
