2feda4b3c0e397fa4b2540b5c1336205c91ad177
Marika <nokelainen.marika@gmail.com>
2017-09-18 12:31:52 +0300

    user class

--- src/model/Buukkeri.java
+++ src/model/Buukkeri.java
@@ -5,7 +5,7 @@ public class Buukkeri {
 	public static void main(String[] args) {
 		DAO_IF test = new DAO();
 		SP pena = new SP("vuorot", "pena", "ronim@metropolia.fi", "0405353169");
-		USER penanasiakas = new USER("Pertti", "Penanen", "yolo2000", )
+		USER penanasiakas = new USER(0001, "Pertti", "Penanen", "yolo2000", "04077777", "perttip@gmail.com");
 		test.createSP(pena);
 
 	}
--- src/model/USER.java
+++ src/model/USER.java
@@ -10,5 +10,41 @@ public class USER {
 	
 	
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 }
