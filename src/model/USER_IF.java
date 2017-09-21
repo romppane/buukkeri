a7e3f6acf426777b13c93d4de409e1d11ffddf78
tommirok <rokolampi2@hotmail.com>
2017-09-21 11:55:12 +0300

    jeaaa

---
+++ src/model/USER_IF.java
@@ -0,0 +1,18 @@
+package model;
+
+public interface USER_IF {
+	public int getId();
+	public void setId(int id);
+	public String getFname();
+	public void setFname(String fname);
+	public String getLname() ;
+	public void setLname(String lname) ;
+	public String getPassword();
+	public void setPassword(String password);
+	public String getPhone() ;
+	public void setPhone(String phone) ;
+	public String getEmail();
+	public void setEmail(String email) ;
+
+	
+}
