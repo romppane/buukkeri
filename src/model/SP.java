6af170e564288f4567f830403ec2299ba34fe095
Ville <Ville@MSI.wlan.metropolia.fi>
2017-10-11 11:47:07 +0300

    javadoc juttuja

--- src/model/SP.java
+++ src/model/SP.java
@@ -1,9 +1,12 @@
 package model;
 
 import java.util.ArrayList;
-
+/**
+ * palvelutuottaja olio
+ * @author Roni, Tommi, Marika, Ville
+ *
+ */
 public class SP implements SP_IF{
-
 	private int id;
 	private String name;
 	private String password;
@@ -14,17 +17,32 @@ public class SP implements SP_IF{
 	private Activity_IF[] activities;
 
 	private ArrayList<Shift_IF[]> providershifts;
-
+/**
+ * tyhjä constructori
+ */
 	public SP() {
 	}
-
+/**
+ * parametrillinen constructori
+ * @param name nimi
+ * @param password salasana
+ * @param email sähköposti
+ * @param phone puhelin numero
+ */
 	public SP(String name, String password, String email, String phone) {
 		this.name = name;
 		this.password = password;
 		this.email = email;
 		this.phone = phone;
 	}
-
+/**
+ * parametrillinen constructori
+ * @param id id numero
+ * @param name nimi
+ * @param password salasana
+ * @param email sähköposti
+ * @param phone puhelin numero
+ */
 	public SP(int id,String name, String password, String email, String phone) {
 		this.id = id;
 		this.name = name;
@@ -34,7 +52,15 @@ public class SP implements SP_IF{
 	}
 
 
-
+/**
+ * parametrillinen constructori
+ * @param id id numero
+ * @param name nimi
+ * @param password salasana
+ * @param email sähköposti
+ * @param phone puhelin numero
+ * @param dao data acceess object
+ */
 	public SP(int id, String name, String password, String email, String phone, DAO_IF dao) {
 		this.id = id;
 		this.name = name;
@@ -43,60 +69,106 @@ public class SP implements SP_IF{
 		this.phone = phone;
 		this.dao = dao;
 	}
-
+/**nimen getteri
+ * @return name
+ */
 	public String getName() {
 		return name;
 	}
+	/**nimen setteri
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**salasanan getteri
+	 * @return password
+	 */
 	public String getPassword() {
 		return password;
 	}
+	/**salasanan setteri
+	 * @param password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	/**sähköpostin getteri
+	 * @return email
+	 */
 	public String getEmail() {
 		return email;
 	}
+	/**sähköpostin setteri
+	 * @param email
+	 */
 	public void setEmail(String email) {
-		this.email = email;
+		// TODO Auto-generated method stub
+
 	}
+	/** puhelin numeron getteri
+	 * @return phone
+	 */
 	public String getPhone() {
 		return phone;
 	}
+	/**puhelin numeron setteri
+	 * @param phone
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
+/**id numeron getteri
+ * @return id
+ */
 	public int getId() {
 		return id;
 	}
-
+/**
+ * palauttaa palvelun tuottajan aktiviteetti listan
+ * @return activities
+ */
 	public Activity_IF[] getActivities() {
 		return activities;
 	}
-
+/**
+ * palauttaa palvelun tuottajan vuorot
+ * @return providershifts
+ */
 	public ArrayList<Shift_IF[]> getProvidershifts() {
 		return providershifts;
 	}
 
-
+/**asettaa uuden data access object olion
+ * @param dao
+ */
 	public void setDao(DAO_IF dao) {
 		this.dao = dao;
 	}
-
+/**
+ * täyttää palveluntuottajan aktiviteetti taulun
+ */
 	public void fillActivities() {
 		activities = dao.readActivitiesBySPId(id);
 	}
-
+/**
+ * täytää palveluntuottajan vuoro listan
+ */
 	public void fillShifts() {
 		providershifts = new ArrayList<>();
 		for(int i = 0; i < activities.length; i++) {
 			providershifts.add(dao.readActivityShifts(activities[i].getId()));
 		}
 	}
-
+/**luo palvelun tuottajan aktiviteettille vuoroja
+ * @param starth
+ * @param startmin
+ * @param endh
+ * @param endmin
+ * @param lenght
+ * @param price
+ * @param act
+ */
 	public void createSetOfShifts(int starth, int startmin, int endh, int endmin, int length, double price, Activity_IF act) {
 		boolean done = true;
 		System.out.println("Looppi alkaa");
@@ -116,7 +188,6 @@ public class SP implements SP_IF{
 
 		}
 	}
-
 
 }
 
--- src/model/User.java
+++ src/model/User.java
@@ -1,7 +1,11 @@
 package model;
 
 import java.util.ArrayList;
-
+/**
+ *käyttäjä olio
+ * @author Roni, Tommi, Marika, Ville
+ *
+ */
 public class User implements User_IF  {
 	private int id;
 	private String fname;
@@ -15,12 +19,21 @@ public class User implements User_IF  {
 	private ArrayList<Shift_IF> shifts;
 	private ArrayList<Activity_IF> activities;
 
-
+/**
+ * tyhjä consturctori
+ */
 	public User() {
 
 	}
 
-
+/**
+ * parametrillinen constructori
+ * @param fname
+ * @param lname
+ * @param password
+ * @param phone
+ * @param email
+ */
 	public User (String fname, String lname, String password, String phone, String email) {
 		this.fname = fname;
 		this.lname = lname;
@@ -29,7 +42,15 @@ public class User implements User_IF  {
 		this.email = email;
 
 	}
-
+/**
+ *parametrillinen constructori
+ * @param id
+ * @param fname
+ * @param lname
+ * @param password
+ * @param phone
+ * @param email
+ */
 	public User(int id, String fname, String lname, String password, String phone, String email) {
 		this.id = id;
 		this.fname = fname;
@@ -38,7 +59,16 @@ public class User implements User_IF  {
 		this.phone = phone;
 		this.email = email;
 	}
-
+/**
+ * parametrillinen constructori
+ * @param id
+ * @param fname
+ * @param lname
+ * @param password
+ * @param phone
+ * @param email
+ * @param dao
+ */
 	public User(int id, String fname, String lname, String password, String phone, String email, DAO_IF dao) {
 		this.id = id;
 		this.fname = fname;
@@ -48,56 +78,103 @@ public class User implements User_IF  {
 		this.email = email;
 		this.dao = dao;
 	}
-
+/**käyttäjän id numeron getteri
+ * @param id
+ */
 	public int getId() {
 		return id;
 	}
+	/**käyttäjän id numeron setteri
+	 * @return id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**ekan nimen getteri
+	 * @return fname
+	 */
 	public String getFname() {
 		return fname;
 	}
+	/**etunimen setteri
+	 * @param fname
+	 */
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
+	/**sukunimen getteri
+	 * @return lname;
+	 */
 	public String getLname() {
 		return lname;
 	}
+	/**sukunimen setteri
+	 * @param lname
+	 */
 	public void setLname(String lname) {
 		this.lname = lname;
 	}
+	/**salasanan getteri
+	 *  @return password
+	 */
 	public String getPassword() {
 		return password;
 	}
+	/**salasanan setteri
+	 * @param password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	/**puhelin numeron getteri
+	 * @return phone
+	 */
 	public String getPhone() {
 		return phone;
 	}
+	/**puhelin numeron setteri
+	 * @param phone
+	 */
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+	/**sähköpostin getteri
+	 * @return email
+	 */
 	public String getEmail() {
 		return email;
 	}
+	/**sähköpostin setteri
+	 * @param email
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	/**
+	 * palauttaa varaus olio-listan
+	 * @return bookings
+	 */
 	public Booking_IF[] getBookings() {
 		return bookings;
 	}
-
+/**
+ * palauttaa käyttäjän vuorot
+ * @return shifts
+ */
 	public ArrayList<Shift_IF> getShifts() {
 		return shifts;
 	}
-
+/**
+ * palauttaa käyttäjän aktiviteetit
+ * @return activities
+ */
 	public ArrayList<Activity_IF> getActivities() {
 		return activities;
 	}
 
-
+/**
+ * täyttää käyttäjän booking taulukon
+ */
 	public void fillBookingData() {
 		// gets bookings array
 		bookings = dao.readBookingsByUserId(id);
